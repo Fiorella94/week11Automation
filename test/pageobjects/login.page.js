a4001a9583d4bbfbbae9cef1e9effb9c8236659e
fiorella94 <fiorella1994_s@hotmail.com>
2021-05-12 19:13:01 -0300

    Changes to login.pages

--- test/pageobjects/login.page.js
+++ test/pageobjects/login.page.js
@@ -1,53 +1,40 @@
 class LoginPage {
-    /*GETTERS*/
-    
-    
-    // get validationContainer () { return $('#validation-container') }
-    
-    
-    /*LABELS*/
-    get labelEmail () { return $$('label')[0] }
-    get labelPassword () { return $$('label')[1] }
-
-    /*EMAIL*/
-    get inputEmail () { return $('#form-login-email') }
-    
-    /*PASSWORD*/
-    get inputPassword () { return $('#form-login-password') }
-
-    /*EMAIL ERROR*/
-    get errorEmail () { return $('#login-error-email') }
-    
-    /*PASSWORD ERROR*/
-     get errorPassword () { return $('#login-error-password') }
-     
-     /*BUTTONS*/
-     get btnRegister () { return $('#back-to-register') }
-    get btnSubmit () { return $('#login-button') }
-   
-      /*SETTERS*/
-    setEmail (email) {
-      this.inputEmail.setValue(email);
-      browser.keys('Tab');
-    }
-    setPassword (password) {
-      this.inputPassword.setValue(password);
-      browser.keys('Tab');
-    }
-    // /*selector for clean form  function*/
-    // get btnFinish {
-    //   return $('input[id="cleanFormLink"]');
-    // }  
-    /* METHODS */
-     
-    buttonRegister () {
-      this.btnRegister.click();
-    }    
-    testLogin (email, password) {
-      this.setEmail(email);
-      this.setPassword(password);
-      this.btnSubmit.click();
-    }
+  /*GETTERS*/
+  // get validationContainer () { return $('#validation-container') }
+  /*LABELS*/
+  get labelEmail () { return $$('label')[0] }
+  get labelPassword () { return $$('label')[1] }
+  /*selector for email field*/
+  get inputEmail () { return $('#form-login-email') }
+  /*selector for password field*/
+  get inputPassword () { return $('#form-login-password') }
+  /*selector for Email error*/
+  get errorEmail () { return $('#login-error-email') }
+  /*selector for Password error*/
+  get errorPassword () { return $('#login-error-password') }    
+  /*selector for the Register button*/
+  get btnRegister () { return $('#back-to-register') }    
+  /*selector for login submit button*/
+  get btnSubmit () { return $('#login-button') }
+  /*list of results*/
+  get listOfResults () { return $('#list-of-results') }  
+  /*SETTERS*/
+  setEmail (email) {
+    this.inputEmail.setValue(email);
+    browser.keys('Tab');
   }
-    
-  module.exports = new LoginPage();
+  setPassword (password) {
+    this.inputPassword.setValue(password);
+    browser.keys('Tab');
+  }    
+  /* METHODS */     
+  buttonRegister () {
+    this.btnRegister.click();
+  }    
+  testLogin (email, password) {
+    this.setEmail(email);
+    this.setPassword(password);
+    this.btnSubmit.click();
+  }
+}    
+module.exports = new LoginPage();
