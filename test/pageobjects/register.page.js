528ed84ba32828bcbfc1d1439312ac38a26308b5
fiorella94 <fiorella1994_s@hotmail.com>
2021-05-12 17:54:24 -0300

    Fixed errors in selectors

--- test/pageobjects/register.page.js
+++ test/pageobjects/register.page.js
@@ -1,49 +1,34 @@
-class ResiterPage {
-    /*GETTERS*/
-    
-  
-    /*LABELS*/
-    get labelName () { return $$('label')[0] }
-    get labelEmail () { return $$('label')[1] }
-    get labelFirstPassword () { return $$('label')[2] }
-    get labelSecondPassword () { return $$('label')[3] }
-  
-  
-    /*NAME*/
-   get inputName () { return $('#form-register-name') }
- 
-   /*EMAIL*/
- get inputEmail () { return $('#form-register-email') }
-   
- /*FIRST PASSWORD*/
+class RegisterPage {
+  /*GETTERS*/
+  /*LABELS*/
+  get labelName () { return $$('label')[0] }
+  get labelEmail () { return $$('label')[1] }
+  get labelFirstPassword () { return $$('label')[2] }
+  get labelSecondPassword () { return $$('label')[3] }
+  /*selector for name field*/ 
+  get inputName () { return $('#form-register-name') }
+  /*selector for email field*/
+   get inputEmail () { return $('#form-register-email') }
+  /*selector for create password  field*/
   get inputFirstPassword () { return $('#form-register-first-password') }
-
-  /* CONFIRM PASSWORD  */
+  /*selector for confirm password  field*/
   get inputSecondPassword () { return $('#form-register-second-password') }
-
- /*NAME ERROR*/
+  /*selector for the Name  error*/  
   get errorName () { return $('#register-error-name') }
-
-  /*EMAIL ERROR*/
+  /*selector for the Email  error*/  
   get errorEmail () { return $('#register-error-email') }
-
- /*ERROR FOR PASSWORDS*/
+  /*selector for the First Password  error*/  
   get errorFirstPassword () { return $('#register-error-first-password') }
+  /*selector for the Second Password  error*/  
   get errorSecondPassword () { return $('#register-error-second-password') }
-
-
-  /*BACK TO LOGIN BUTTON*/
+  /*selector for the back to login button*/  
   get btnLogin () { return $('#back-to-login') }
-  
-
- /*RESET FIELDS BUTTON*/
+  /*selector for the Reset fields  button*/  
   get btnReset () { return $('#reset-fields-button') }
-
-  /*SUBMIT BUTTON*/
+  /*selector for the Submit button*/  
   get btnSubmit () { return $('#submit-button') }
-
-
-
+  /*list of results*/
+  get listOfResults () { return $('#list-of-results') }  
   /*SETTERS*/
   setName(name) {
     this.inputName.setValue(name);
@@ -61,30 +46,19 @@ class ResiterPage {
     this.inputSecondPassword.setValue(secondPassword);
     browser.keys('Tab');
   }
-
   /*methods*/
-
   backToLogin () {
       this.btnLogin.click();
   }
-
   cleanForm () {
     this.btnReset.click();
-  }
-
-  
-
-
-  
+  }  
   testRegister(name, email, firstPassword, secondPassword) {
-
     this.setName(name);
     this.setEmail (email);
     this.setFirstPassword (firstPassword);
     this.setSecondPassword (secondPassword);
     this.btnSubmit.click();
-  }
-    
+  }    
 }
-
 module.exports = new RegisterPage();
--- test/specs/registertest.js
+++ test/specs/registertest.js
@@ -7,72 +7,124 @@ describe('Register section', () => {
     const urlRegister = 'https://fiorella94.github.io/';
     const urlLogin = 'https://fiorella94.github.io/index2.html';
 
-    // Email Tests
-    it('input with empty email', () => {
+    beforeAll("Open browser on register url", () => {
         browser.url(urlRegister);
-        RegisterPage.register('','fiorella','zzz','123');
-        expect(RegisterPage.registerErrorDivMail).toBe("Email field can't be empty");
-        browserPause();
-    });
-    it('input with email invalid', () => {
-        RegisterPage.register('fiorella@', 'fiorella', 'fi', 'f');
-        expect(RegisterPage.registerErrorDivMail).toBe('Email is invalid');
-        browserPause();
-    });
-    // Name tests
-    it('input name empty', () => {
-        RegisterPage.register('fiorella1994@hotmail.com','','zzz','123');
-        expect(RegisterPage.registerErrorDivName).toBe("Name field can't be empty");
-        browserPause();
-    });
-    it('input name invalid', () => {
-        RegisterPage.register('fiorella1994@hotmail.com', 'fiore', '1122', '1122');
-        expect(RegisterPage.registerErrorDivName).toBe('Full name must contains a space');
-        browserPause();
-    });
-     // Password tests
-     it('password without capital letter ', () => {
-        RegisterPage.register('fiorella1994@hotmail.com', 'fiorella salas', '1122', '1122');
-        expect(RegisterPage.registerErrorDivPassword).toBe('Password must contain at least one uppercase letter');
-        browserPause();
-    });
-    it('password without capital letter ', () => {
-        RegisterPage.register('fiorella1994@hotmail.com', 'fiorella salas', 'A1122', 'A1122');
-        expect(RegisterPage.registerErrorDivPassword).toBe('Password must contain at least one lowercase letter');
-        browserPause();
-    });
-    it('password with at least 8 characters', () => {
-        RegisterPage.register('fiorella1994@hotmail.com', 'fiorella salas', 'Always1', 'Always1');
-        expect(RegisterPage.registerErrorDivPassword).toBe('Password must have at least 8 characters');
-        browserPause();
-    });
-    it('password without number', () => {
-        RegisterPage.register('fiorella1994@hotmail.com', 'fiorella salas', 'Computer', 'Alberdil');
-        expect(RegisterPage.registerErrorDivPassword).toBe('Password must contain at least one number');
-        browserPause();
-    });
-    //Password confirm tests
-    it('input confirm password empty', () => {
-        RegisterPage.register('fiorella1994@hotmail.com', 'fiorella salas', 'Always12', '');
-        expect(RegisterPage.registerErrorDivConfirmPassword).toBe("confirm password field can't be empty");
-        browserPause();
-    });
-    it('input confirm password not match with password', () => {
-        RegisterPage.register('fiorella1994@hotmail.com', 'fiorella salas', 'Always12', 'alw12');
-        expect(RegisterPage.registerErrorDivConfirmPassword).toBe('passwords must match');
-        browserPause();
-    });
-     // register with valid credentials
-     it('inputs with valid credentials', () => {
-        RegisterPage.register('fiorella1994@hotmail.com', 'fiorella salas', 'Always12', 'Always12');
-        expect(RegisterPage.registerErrorDivMail).toBe('');
-        browserPause();
-    });
-
-    // Back To login
-    it('Back to login link test', () => {
-        RegisterPage.BackToLogin();
-        expect(browser).toHaveUrl(urlLogin);
-        browserPause();
+      });
+    describe ('Name, Email and both Passwords fields testing', () => {
+        /*NAME*/
+        it('empty name', () => {
+            RegisterPage.setName()            
+            expect(RegisterPage.errorName).toHaveText("Name must be at least 6 characters long and have a space");
+            browser.pause(2000);
+        });
+        it('name with no spaces in between', () => {
+            RegisterPage.setName('fiosalas');
+            expect(RegisterPage.errorName).toHaveText("Name must be at least 6 characters long and have a space");
+            browser.pause(2000);
+        });
+        it('name  shorter than 6 characters long', () => {
+            RegisterPage.setName('fio s');
+            expect(RegisterPage.errorName).toHaveText("Name must be at least 6 characters long and have a space");
+            browser.pause(2000);
+        });
+        it('Name correctly formated', () => {
+            RegisterPage.setName('Fiorella Salas');
+            expect(RegisterPage.errorName).toHaveText("");
+            browser.pause(2000);
+        });
+         /*MAIL*/
+         it('empty mail', () => {
+            RegisterPage.setEmail();
+            expect(RegisterPage.errorEmail).toHaveText("E-mail must have a valid format");
+            browser.pause(2000);
+        });
+        it('wrongly formated email no @', () => {
+            RegisterPage.setEmail('fiogmail.com');
+            expect(RegisterPage.errorEmail).toHaveText("E-mail must have a valid format");
+            browser.pause(2000);
+        });
+        it('corerctly formated email', () => {
+            RegisterPage.setEmail('fiorella2994s@gmail.com');
+            expect(RegisterPage.errorEmail).toHaveText("");
+            browser.pause(2000);
+        });
+        /*FIRST PASSWORD*/
+        it('First password empty', () => {
+            RegisterPage.setFirstPassword();
+            expect(RegisterPage.errorFirstPassword).toHaveText("Password must have the correct format");
+            browser.pause(2000);
+        });
+        it('First password wrong format: less than 8 characters', () => {
+            RegisterPage.setFirstPassword('always');
+            expect(RegisterPage.errorFirstPassword).toHaveText("Password must have the correct format");
+            browser.pause(2000);
+        });
+        it('First password wrong format: only letters', () => {
+            RegisterPage.setFirstPassword('aaaaaaaa');
+            expect(RegisterPage.errorFirstPassword).toHaveText("Password must have the correct format");
+            browser.pause(2000);
+        });
+        it('First password wrong format: only numbers', () => {
+            RegisterPage.setFirstPassword('11112222');
+            expect(RegisterPage.errorFirstPassword).toHaveText("Password must have the correct format");
+            browser.pause(2000);
+        });
+        it('First password with a valid format', () => {
+            RegisterPage.setFirstPassword('Always12');
+            expect(RegisterPage.errorFirstPassword).toHaveText("");
+            browser.pause(2000);
+        });
+        /*SECOND PASSWORD*/
+        it('Second password empty', () => {
+            RegisterPage.setSecondPassword();
+            expect(RegisterPage.errorSecondPassword).toHaveText("Passwords must have the correct format and match");
+            browser.pause(2000);
+        });
+        it('Second password wrong format: less than 8 characters', () => {
+            RegisterPage.setSecondPassword('a12');
+            expect(RegisterPage.errorSecondPassword).toHaveText("Passwords must have the correct format and match");
+            browser.pause(2000);
+        });
+        it('Second password wrong format: only letters', () => {
+            RegisterPage.setSecondPassword('aaaawwws');
+            expect(RegisterPage.errorSecondPassword).toHaveText("Passwords must have the correct format and match");
+            browser.pause(2000);
+        });
+        it('Second password wrong format: only numbers', () => {
+            RegisterPage.setSecondPassword('1232222');
+            expect(RegisterPage.errorSecondPassword).toHaveText("Passwords must have the correct format and match");
+            browser.pause(2000);
+        });
+        it('Second password correctly formated but not matching the first password', () => {
+            RegisterPage.setSecondPassword('Always122');
+            expect(RegisterPage.errorSecondPassword).toHaveText("Passwords must have the correct format and match");
+            browser.pause(2000);
+        });
+        it('Second password correctly formated and matching the first password', () => {
+            RegisterPage.setSecondPassword('Always12');
+            expect(RegisterPage.errorSecondPassword).toHaveText("");
+            browser.pause(2000);
+        });
+        it('After inserting all the valid credentials on all four fields, click the submit'+
+        'button and test if they show on the validations section below', () => {
+            RegisterPage.testRegister('Fiorella Salas', 'fiorella2994s@gmail', 'Always12', 'Always12' )
+            expect(RegisterPage.listOfResults).toHaveTextContaining([
+                "Every validation has passed", 
+                
+            ]);
+            browser.pause(2000);
+        });
+        
+        
+        it('Press Back to Login button and check if the URL is correct', () => {
+            RegisterPage.btnLogin.click();
+            expect(browser).toHaveUrl(
+                'https://fiorella94.github.io/index2.html'
+            );
+            expect(browser).toHaveTitle("LOGIN");
+            browser.pause(2000);
+        });
     });
 });
+
+        
